845e9a69f57c6b8bc5f5a2eafabbac3c3e8cfafd
John Fitzpatrick <john.fitzpatrick.v@gmail.com>
2016-04-17 23:07:28 -0400

    Create utility.h

---
+++ CS_325_Project1/utility.h
@@ -0,0 +1,8 @@
+/*  Fills an array from a file that contains integers in
+    array format, i.e. [5, 4, -1,...]
+    params:
+        size:       the amount of numbers to be in the array
+    returns:
+        arr:        pointer to the array that was generated
+*/
+int *randomArray(int);
